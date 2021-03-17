ef61616fc114b31a38042ada4d15433a728414e6
wenju11 <814438839@qq.com>
2021-03-17 21:42:25 +0800

    解决识别不了datasource url 问题

--- Snowdao/src/main/java/com/cloud/snow/Bootstrap.java
+++ Snowdao/src/main/java/com/cloud/snow/Bootstrap.java
@@ -2,8 +2,9 @@ package com.cloud.snow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Bootstrap {
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class,args);
--- Snowservice/src/main/java/com/cloud/snow/Bootstrap.java
+++ Snowservice/src/main/java/com/cloud/snow/Bootstrap.java
@@ -2,8 +2,8 @@ package com.cloud.snow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
 public class Bootstrap {
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class,args);
